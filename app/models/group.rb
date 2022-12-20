3542f56122d359ae5367d3e383a88e51f8da4705
Carlos Vivas <carlosmuerto@gmail.com>
2022-12-20 03:28:45 +0000

    Add model validation

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,8 @@
 class Group < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User', foreign_key: 'user_id'
+  has_many :group_entity
+  has_many :entities, through: :group_entity, source: :entity
+
+  validates :name, presence: true
+  validates :icon, presence: true
 end
