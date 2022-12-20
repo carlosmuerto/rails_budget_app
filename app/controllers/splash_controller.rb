465b410e50f74bfbb672a9afca0b1bf7094e980e
Carlos Vivas <carlosmuerto@gmail.com>
2022-12-20 00:57:50 +0000

    delete commented code

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -3,7 +3,6 @@ class SplashController < ApplicationController
   before_action :check_signed_in
 
   def check_signed_in
-    puts '------------ if signed_in? ----------- ' if signed_in?
     redirect_to users_path if signed_in?
   end
 
