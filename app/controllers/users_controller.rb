5f2fc2e142beb70af668e1da9abc0153417e3ffa
Carlos Vivas <carlosmuerto@gmail.com>
2022-12-20 13:59:39 +0000

    Add cancancan

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,22 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user, only: %i[show edit update destroy]
+
+  load_and_authorize_resource
 
   # GET /users or /users.json
-  def index
-    @users = User.all
-  end
+  def index; end
 
   # GET /users/1 or /users/1.json
   def show; end
 
   # GET /users/new
-  def new
-    @user = User.new
-  end
+  def new; end
 
   # GET /users/1/edit
   def edit; end
 
   # POST /users or /users.json
   def create
-    @user = User.new(user_params)
-
     respond_to do |format|
       if @user.save
         format.html { redirect_to user_url(@user), notice: 'User was successfully created.' }
@@ -59,9 +54,7 @@ class UsersController < ApplicationController
   private
 
   # Use callbacks to share common setup or constraints between actions.
-  def set_user
-    @user = User.find(params[:id])
-  end
+  def set_user; end
 
   # Only allow a list of trusted parameters through.
   def user_params
---
+++ app/models/ability.rb
@@ -0,0 +1,41 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    return unless user.present?
+
+    can %i[read update], User, id: user.id
+    can %i[read update create destroy], Group, author: user.id
+    can %i[read update create destroy], Entity, author: user.id
+    can %i[read update create destroy], GroupEntity
+    # can %i[read update create destroy], GroupEntity, [
+    #   group: { user: },
+    #   entity: { user: }
+    # ]
+
+    # Define abilities for the user here. For example:
+    #
+    #   return unless user.present?
+    #   can :read, :all
+    #   return unless user.admin?
+    #   can :manage, :all
+    #
+    # The first argument to `can` is the action you are giving the user
+    # permission to do.
+    # If you pass :manage it will apply to every action. Other common actions
+    # here are :read, :create, :update and :destroy.
+    #
+    # The second argument is the resource the user can perform the action on.
+    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
+    # class of the resource.
+    #
+    # The third argument is an optional hash of conditions to further filter the
+    # objects.
+    # For example, here the user can only update published articles.
+    #
+    #   can :update, Article, published: true
+    #
+    # See the wiki for details:
+    # https://github.com/CanCanCommunity/cancancan/blob/develop/docs/define_check_abilities.md
+  end
+end
