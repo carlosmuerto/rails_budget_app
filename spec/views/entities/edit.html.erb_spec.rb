1c265b6750ab1c269608077da99a475e5ec74544
Carlos Vivas <carlosmuerto@gmail.com>
2022-12-21 20:04:18 +0000

    entity view test

--- spec/views/entities/edit.html.erb_spec.rb
+++ spec/views/entities/edit.html.erb_spec.rb
@@ -1,16 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe 'entities/edit', type: :view do
-  let(:entity) do
-    Entity.create!(
-      name: 'MyString',
-      amount: '9.99',
-      user: nil
-    )
+  let!(:user) do
+    test_user
+  end
+
+  let!(:groups) do
+    [
+      test_group(user, 'A'),
+      test_group(user, 'B')
+    ]
+  end
+
+  let!(:entity) do
+    test_entity(user, 'A', groups)
   end
 
   before(:each) do
     assign(:entity, entity)
+    assign(:groups, Group.all)
+    sign_in user
+    render
   end
 
   it 'renders the edit entity form' do
@@ -20,8 +30,8 @@ RSpec.describe 'entities/edit', type: :view do
       assert_select 'input[name=?]', 'entity[name]'
 
       assert_select 'input[name=?]', 'entity[amount]'
-
-      assert_select 'input[name=?]', 'entity[user_id]'
     end
   end
+
+  it 'chech for print more'
 end
--- spec/views/entities/show.html.erb_spec.rb
+++ spec/views/entities/show.html.erb_spec.rb
@@ -1,18 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe 'entities/show', type: :view do
+  let!(:user) do
+    test_user
+  end
+
+  let!(:groups) do
+    [
+      test_group(user, 'A'),
+      test_group(user, 'B')
+    ]
+  end
+
+  let!(:entity) do
+    test_entity(user, 'A', groups)
+  end
+
   before(:each) do
-    assign(:entity, Entity.create!(
-                      name: 'Name',
-                      amount: '9.99',
-                      user: nil
-                    ))
+    assign(:entity, entity)
+    sign_in user
+    render
   end
 
   it 'renders attributes in <p>' do
-    render
-    expect(rendered).to match(/Name/)
-    expect(rendered).to match(/9.99/)
-    expect(rendered).to match(//)
+    expect(rendered).to match(/#{entity.name}/)
+    expect(rendered).to match(/#{entity.amount}/)
   end
+
+  it 'chech for print more'
 end
