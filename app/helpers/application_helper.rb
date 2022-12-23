1aa6955fd16edd95c3428148faf5513b9169fe5f
Carlos A. Vivas N <cavivasnieto@gmail.com>
2022-12-23 04:42:28 +0000

    Add Entity preview

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,13 @@
 module ApplicationHelper
   def go_back_path
-    return group_path(Group.find(params[:group_id])) if current_page?(controller: 'entities',
-                                                                      action: 'new') && !current_page?(new_entity_path)
+    return group_path(Group.find(params[:group_id])) if current_page_is_entity_with_group_path?
 
     root_path
+  end
+
+  private
+
+  def current_page_is_entity_with_group_path?
+    current_page?(controller: 'entities', action: 'new') && !current_page?(new_entity_path)
   end
 end
