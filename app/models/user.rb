5b5a5eb2e279fab04b913300b7eecf818767b5ce
Carlos A. Vivas N <cavivasnieto@gmail.com>
2023-04-01 23:40:27 -0400

    minor fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable :recoverable,
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :rememberable, :validatable
 
   has_many :entities
   has_many :groups
